05c1e159e4707e31ca670c6bb12795c10d678c51
aleee011420{ <ghali786786@gmail.com>
2023-08-25 20:54:14 +0530

    first created

---
+++ introduction/first.js
@@ -0,0 +1,4 @@
+console.log("in my introductory part, there is something i want to tell")
+console.log("my name is ali mohammad malla")
+console.log("i am the son of haji shabir ahmad malla")
+
---
+++ test.js
@@ -0,0 +1,1 @@
+console.log("i am the son of haji shabir ahmad malla")
