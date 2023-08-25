50a4c00f5ae94ddb940d9ca7927fc8a20ae8f121
aleee011420{ <ghali786786@gmail.com>
2023-08-25 21:01:31 +0530

    feature added

---
+++ introduction/academics.js
@@ -0,0 +1,3 @@
+ console.log("currently i am pursuing my btech in computer science and engineering in sviet college of engineering")
+console.log("i have only one goal in my life, i.e to earn billions of dollars of money")
+
